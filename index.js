1cf69e0ff0ad64d9f46bcf6ae6cfd239ed107abd
Jacob David Diehl <jakediehl17@gmail.com>
2022-12-19 11:29:19 -0700

    commit tree 2521440dad3273740dabe99f1416e315147f26eb

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+
+
+
+
+
+
+function superbowlWin(record) {
+    const result = record.find( function (record){ return record.result === "W" });
+    return result ? result.year : undefined;
+}
+
+
+
+
+console.log(superbowlWin(record));
+
+
+
+
+
+
